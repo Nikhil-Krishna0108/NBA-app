e3095497662338d61bdd83a8c22114ad0907b2bf
Barebones00 <nikhilgooty@gmail.com>
2022-05-28 17:06:12 +0530

    Added db.js file

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose"); 
+
+require('dotenv').config();
+const uri = process.env.URI;
+const options = {
+    keepAlive:true, useNewUrlParser: true, useUnifiedTopology: true
+}
+mongoose.connect(uri, options).catch((error) => console.log(error));
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+	console.log('MongoDB database connection established successfully');
+});
+
+module.exports = Mongoconnection;
