672b6617524eb5266d97a73fea3f89d6a2f6f11e
Barebones00 <nikhilgooty@gmail.com>
2022-05-28 17:07:50 +0530

    Modified server.js

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,13 @@
 const express = require('express')
 const app = express()
-const mongoose = require("mongoose"); 
+const Mongoconnection = require('./db')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 const cors = require('cors')
 app.use(cors())
 
-require('dotenv').config();
-const uri = process.env.URI;
-const options = {
-    keepAlive:true, useNewUrlParser: true, useUnifiedTopology: true
-}
-mongoose.connect(uri, options).catch((error) => console.log(error));
-
-const connection = mongoose.connection;
-connection.once('open', () => {
-	console.log('MongoDB database connection established successfully');
-});
-
-// async function connect()
-// { 
-//     client.connect(err => {
-//         if(!err){
-//             console.log('Connected to MongoDB');
-//         }
-//         else{
-//             console.log('Error connecting to MongoDB'+err);
-//         }
-//     })
-// }
-
-//DB connection call
-//connect()
+Mongoconnection();
 
 app.listen(5000, ()=>{
     console.log(`Successfully served on port: 5000`);
@@ -41,12 +16,4 @@ app.listen(5000, ()=>{
 const criteria1 = require('./controllers/criteria1.controller')
 app.use('/criteria1', criteria1)
 
-// app.get('/', (req, res) => {
-//     res.send('Server is running.')
-// })
-
-// app.post('/criteria1/new', (req, res) => {
-//     client.db('2022').collection('criteria1').insertOne(req.body)
-//     .then(result => res.json(result))
-// })
 
